62463def610b22435f0e96dfb75f13c207a2198f
Björn Dahlgren <bjodah@gmail.com>
2016-03-11 16:20:45 +0100

    Update block_diag_ilu recipe

--- recipes/30_python-numpy/block_diag_ilu/build.sh
+++ recipes/30_python-numpy/block_diag_ilu/build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-export LIBRARY_PATH=$PREFIX/lib
-export INCLUDE_PATH=$PREFIX/include
-export CPLUS_INCLUDE_PATH=$PREFIX/include
-${PYTHON} setup.py build
+LLAPACK=openblas ${PYTHON} setup.py build
 ${PYTHON} setup.py install
